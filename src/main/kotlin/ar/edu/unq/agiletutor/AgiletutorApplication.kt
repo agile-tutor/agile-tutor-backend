dd6e308b007bdf659d236ba4b423c6611b53caaa
AlexEnigmaCode <alegfprogramador@gmail.com>
2023-03-22 15:33:25 -0300

    backbone del proyecto

---
+++ src/main/kotlin/ar/edu/unq/agiletutor/AgiletutorApplication.kt
@@ -0,0 +1,11 @@
+package ar.edu.unq.agiletutor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgiletutorApplication
+
+fun main(args: Array<String>) {
+	runApplication<AgiletutorApplication>(*args)
+}
---
+++ src/test/kotlin/ar/edu/unq/agiletutor/AgiletutorApplicationTests.kt
@@ -0,0 +1,13 @@
+package ar.edu.unq.agiletutor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AgiletutorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
